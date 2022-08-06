99de41f739249ee8a5e3b68d104cc323210fb7f2
hvnuoc <108932131+hvnuoc@users.noreply.github.com>
2022-08-06 09:52:33 +0700

    create UI redux with antd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Divider } from "antd";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import Filters from "./components/Filters";
+import React from "react";
+const { Title } = Typography;
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div
+            style={{
+                width: 500,
+                margin: "0 auto",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "white",
+                padding: 20,
+                boxShadow: "0 0 10px 4px #bfbfbf",
+                borderRadius: 5,
+                height: "90vh",
+            }}
+        >
+            <Title style={{ textAlign: "center" }}>TODO APP with REDUX</Title>
+            <Filters />
+            <Divider />
+            <TodoList />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
-  }
 }
 
 export default App;
